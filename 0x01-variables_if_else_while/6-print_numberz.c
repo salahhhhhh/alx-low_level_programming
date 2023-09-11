86711dd64bce452bbe50cf476cc5b2f108b7c7ce
salahhhhhh <salahmobarek123@gmail.com>
2023-09-11 08:44:59 -0700

    06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+
+        i = 0;
+        while (i < 10)
+        {
+                putchar (i % 10 + 48);
+                i++;
+        }
+        putchar ('\n');
+        return (0);
+}
