f4522626a2800b198a676c545c46f7cd0575c9a1
salahhhhhh <salahmobarek123@gmail.com>
2023-09-11 08:47:31 -0700

    06

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        while (i < 10)
-        {
-                putchar (i % 10 + 48);
-                i++;
-        }
-        putchar ('\n');
-        return (0);
+	i = 0;
+	while (i < 10)
+	{
+		putchar (i % 10 + 48);
+		i++;
+	}
+	putchar ('\n');
+	return (0);
 }
