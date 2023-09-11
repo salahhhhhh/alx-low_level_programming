56eb59e97240d3026f0d0b20db3e9376f7d0e60f
salahhhhhh <salahmobarek123@gmail.com>
2023-09-10 17:45:19 -0700

    02

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
-	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
-	{
+{
+	for(char ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-		putchar('\n');
-	}
+	putchar('\n');
 	return (0);
 }
