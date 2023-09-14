8ae963aa77d82eb9bb9d07721ac73c5b6f721cfd
salahhhhhh <salahmobarek123@gmail.com>
2023-09-13 17:47:05 -0700

    07

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* hello
+* */
+int print_last_digit(int n)
+{
+	int i;
+
+	if n < 0)
+		n = -n;
+	i = n % 10;
+	if (i < 0)
+		i = -i;
+	_putchar (i + 48);
+	return (i);
+}
+
