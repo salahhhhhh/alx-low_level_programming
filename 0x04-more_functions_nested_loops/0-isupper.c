cbee9c61d1d3f0f1c318556e5fd637ce1e1f1949
salahhhhhh <salahmobarek123@gmail.com>
2023-09-15 14:36:41 -0700

    00

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,6 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return (0);
-}
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
