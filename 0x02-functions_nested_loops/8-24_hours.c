f819434f2181f7ebae784912ecec7cc64d4be673
salahhhhhh <salahmobarek123@gmail.com>
2023-09-16 09:54:05 -0700

    08

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,16 +10,17 @@ void jack_bauer(void)
 	i = 0;
 	while (i < 24)
 	{
-	j = 0;
-	while (j < 60)
-	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
-		_putchar((j / 10) + '0');
-		_putchar((j % 10) + '0');
-		_putchar('\n');
-		j++;
+		j = 0;
+		while (j < 60)
+		{	
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
 	}
-	i++;
 }
